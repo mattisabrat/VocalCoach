cd3c8560ef99841ca716fe6f46efa3706eaf67e4
Matthew Davenport <mdavenport@v-virtual15.rockefeller.edu>
2020-01-03 11:02:26 -0500

    Added a package installer script because I'm hella lazy

---
+++ Install_Packages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#update pip
+python3 -m pip install --upgrade pip
+
+#do the packages
+python3 -m pip install PyAudio==0.2.11
+python3 -m pip install RPI.GPIO==0.6.5
+python3 -m pip install colorzero==1.1
+python3 -m pip install dbus-python==1.2.8
+python3 -m pip install decorator==4.4.0
+python3 -m pip install evento==1.0.1
+python3 -m pip install gpiozero==1.5.1
+python3 -m pip install numpy==1.16.4
+python3 -m pip install omxplayer-wrapper==0.3.2
+python3 -m pip install pathlib2==2.3.4
+python3 -m pip install transitions==0.6.9
+python3 -m pip install yolk3k==0.9
+python3 -m pip install six==1.12.0
